c71f5a023acddcf6f00d07f95a7217418f0e3e64
Jack Maney <jackmaney@gmail.com>
2013-12-19 14:23:54 -0600

    Torus Distance function fixed (forgot some cases)

--- src/com/jackmaney/javaplexfiddle/section41/Exercise4.java
+++ src/com/jackmaney/javaplexfiddle/section41/Exercise4.java
@@ -1,7 +1,5 @@
 package com.jackmaney.javaplexfiddle.section41;
 
-import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
-
 public class Exercise4 {
 
 	public static double[][] randomPointsFromUnitSquare(int n){
@@ -21,11 +19,6 @@ public class Exercise4 {
 	
 	public static double torusDistance(double[] p,double[] q){
 		if(p.length != 2 || q.length != 2){
-//			System.out.println("wtf");
-//			for(int i = 0; i < p.length; i++){
-//				System.out.println(p[i]);
-//			}
-//			System.out.println("hmmm...");
 			throw new IllegalArgumentException();
 		}
 		
@@ -39,59 +32,95 @@ public class Exercise4 {
 		 * or are on either a horizontal or vertical line.
 		 */
 		
-		double deltaX = Math.abs(x1 - x2);
-		double deltaY = Math.abs(y1 - y2);
+		double deltaX = x2 - x1;
+		double deltaY = y2 - y1;
 		
 		if(deltaX == 0 && deltaY == 0){
 			return 0;
 		}
-		else if(deltaX == 0){ //horizontal
-			return Math.max(deltaY, 1 - deltaY);
+		else if(deltaX == 0){ 
+			return Math.max(Math.abs(deltaY), 1 - Math.abs(deltaY));
 		}
-		else if(deltaY == 0){ //vertical
-			return Math.max(deltaX, 1 - deltaX);
+		else if(deltaY == 0){ 
+			return Math.max(Math.abs(deltaX), 1 - Math.abs(deltaX));
 		}
-		else{
-			/*
-			 * Now, we can assume that we have an upper-right point
-			 * and a lower-left point. WLOG, assume they're p and q (resp).
-			 * 
-			 * Now, let d denote the usual Euclidean distance. If y2 - y1
-			 * is at least 1/2, we apply the shifting transformation
-			 * y -> y + (1 - y2) to get new points P'(x1,y1 + 1 - y2)
-			 * and Q'(x2,0). (Note that since y2 - y1 >= 1/2, we have
-			 * y1 - y2 + 1 <= 1/2.) 
-			 * 
-			 * Denoting by dy the distance between P' and Q' (ie the 
-			 * distance on the torus going through the "top" and "bottom" 
-			 * of the square), our condition of y2 - y1 <= 1/2 
-			 * forces d <= dy; in fact, y2 - y1 <= 1/2 if and only if 
-			 * d <= dy. 
-			 * 
-			 * Replacing y with x--and getting an associated distance dx--we
-			 * see that x2 - x1 <= 1/2 if and only if d <= dx.
-			 * 
-			 */
-			double d = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-			double dy = Math.sqrt(deltaX * deltaX + (1-deltaY)*(1-deltaY));
-			double dx = Math.sqrt((1-deltaX)*(1-deltaX) + deltaY*deltaY);
-			//System.out.println("d,dx,dy = " + d + ", " + dx + ", " + dy);
+		
+		/*
+		 * We now have two possibilities for our two points within our square:
+		 * Either there's an upper-left and lower-right point or
+		 * there's a lower-left and upper-right point (ie deltaY / deltaX is
+		 * either positive or negative).
+		 * 
+		 * To prevent further code branching, we'll check to see if the
+		 * slope is negative and if so, apply the horizontal shift
+		 * 				x |-> x + 1 - (lower-right x coordinate)
+		 * and start over. (Note that we could have just as easily
+		 * shifted vertically)
+		 */
+		
+		if((deltaX < 0 && deltaY > 0) || (deltaX > 0 && deltaY < 0)){
+			double[] lowerRight = x1 < x2 ? q : p;
+			double[] upperLeft = x2 < x1 ? q : p;
 			
-			if(deltaX <= 0.5 && deltaY <= 0.5){
-				return d;
-			}
-			else if(deltaY > 0.5 && deltaX<=0.5){
-				return dy;
-			}
-			else if(deltaY <= 0.5 && deltaX > 0.5){
-				return dx;
-			}
-			else{
-				return Math.min(dx,dy);
-			}
+			double[] shiftedLR = {0,lowerRight[1]};
+			double[] shiftedUL = {upperLeft[0] + 1 - lowerRight[0]
+					,upperLeft[1]};
 			
+			return torusDistance(shiftedLR, shiftedUL);
 			
 		}
+		
+		/*
+		 * Now, we have deltaX and deltaY both of the same sign.
+		 * If they're both negative, then p is to the upper-right
+		 * and q is to the upper-left. Let's flip that around, shall we?
+		 */
+		
+		if(deltaX < 0 && deltaY < 0){
+			return torusDistance(q, p);
+		}
+		
+		/*
+		 * Now we can finally assume that p is to the lower-left and
+		 * q is to the upper-right.
+		 * 
+		 * Since we're on a torus, we have three different numbers to compare:
+		 * 
+		 * 		* d, the normal Euclidean distance between q and p.
+		 * 		* dx, the Euclidean distance after shifting q over to the
+		 * 				left side of the square via the transformation
+		 * 				x |-> x + 1 - x2
+		 * 		* dy, the Euclidean distance after shifting q vertically
+		 * 				to the bottom of the square via the transformation
+		 * 				y |-> y + 1 - y2
+		 * 
+		 * Note that since x1 < x2 and y1 < y2, these transformations don't 
+		 * wrap p around the torus (ie x1 + 1 - x2 and y1 + 1 -y2 are still
+		 * less than 1).
+		 * 
+		 * We already know that d = sqrt(deltaX^2 + deltaY^2).
+		 * 
+		 * For dx, our points become P'(x1 + 1 - x2,y1) = (1-deltaX,y1)
+		 * and Q'(0,y2). So, dx = sqrt((1 - deltaX)^2 + deltaY^2).
+		 * 
+		 * Similarly, dy = sqrt(deltaX^2 + (1 - deltaY)^2).
+		 */
+		
+		double d = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+		double dx = Math.sqrt(
+					(1 - deltaX)*(1-deltaX) + deltaY * deltaY
+				);
+		double dy = Math.sqrt(
+					deltaX * deltaX + (1-deltaY)*(1-deltaY)
+				);
+		
+		return Math.min(Math.min(d,dx), dy);
+		
+		/*
+		 * Fun trivia fact: d <= dx iff deltaX <= 1/2 and
+		 * d <= dy iff deltaY <= 1/2.
+		 */
+			
 	}
 	
 	public static double[][] getDistanceMatrix(double[][] points){
@@ -132,7 +161,6 @@ public class Exercise4 {
 			
 			System.out.println(sb.toString());
 		}
-		
 
 	}
 
